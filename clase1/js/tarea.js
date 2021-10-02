8af1bfcaf1265a919f011204154ded4b826c70ff
odonML <odon_ml@outlook.com>
2021-10-02 00:40:16 -0500

    tarea solucion don key dinamica

--- clase1/js/tarea.js
+++ clase1/js/tarea.js
@@ -8,36 +8,66 @@
 // salida de la funcion: '$1500MXN (moneda local elegida por el koder) equivale a $750USD(moneda del pais ingresado por el usuario)'
 // Considerar minimo 4 tipo de monedas
 
-function convert(monedaEx, pesos, sim){
-    let cambio = pesos/monedaEx;
-    return console.log(`${pesos} MXM equivalen a ${sim}${cambio.toFixed(2)}`);
+
+//========SOLUCION 1===========================
+// function convert(monedaEx, pesos, sim){
+//     let cambio = pesos/monedaEx;
+//     return console.log(`${pesos}MXM equivalen a ${sim}${cambio.toFixed(2)}`);
+// }
+// function casaDeCambio(){
+//     let pesos = Number(prompt("introduce la cantidad que deseas convertir?"));
+//     let pais = prompt("escribe uno de estos paises, USA, EU ó UK?");
+//     let sim = "";
+//     switch(pais){
+//         case "Estados Unidos":
+//         case "USA":
+//             let dolar = 20.45;
+//             sim = "$";
+//             convert(dolar, pesos, sim);
+//         break;
+//         case "Europa":
+//         case "EU":
+//             let euro = 23.71;
+//             sim = "€";
+//             convert(euro, pesos, sim);
+//         break;
+//         case "Reino Unido":
+//         case "UK":
+//             let libra = 27.70;
+//             sim = "£";
+//             convert(libra, pesos, sim);
+//         break;
+//         default:
+//             console.log("pais no valido");
+//     }
+// }
+// casaDeCambio();
+
+//================SOLUCION 2==========================
+function convert({moneda, simbolo, pesos}){
+    let cambio = pesos/moneda;
+    return console.log(`${pesos} MXM equivalen a ${simbolo}${cambio.toFixed(2)}`);
 }
 function casaDeCambio(){
     let pesos = Number(prompt("introduce la cantidad que deseas convertir?"));
     let pais = prompt("escribe uno de estos paises, USA, EU ó UK?");
-    let sim = "";
-    switch(pais){
-        case "Estados Unidos":
-        case "USA":
-            let dolar = 20.45;
-            sim = "$";
-            convert(dolar, pesos, sim);
-        break;
-        case "Europa":
-        case "EU":
-            let euro = 23.71;
-            sim = "€";
-            convert(euro, pesos, sim);
-        break;
-        case "Reino Unido":
-        case "UK":
-            let libra = 27.70;
-            sim = "£";
-            convert(libra, pesos, sim);
-        break;
-        default:
-            console.log("pais no valido");
+    let paises = {
+        USA: {
+            moneda: 20.45,
+            simbolo: "$",
+            pesos
+        },
+        EU: {
+            moneda: 23.71,
+            simbolo: "€",
+            pesos
+        },
+        UK: {
+            moneda: 27.70,
+            simbolo: "£",
+            pesos
+        },
     }
+    convert(paises[pais]);
 }
-
 casaDeCambio();
