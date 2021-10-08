aa2817b0f05efdcdaff43d3f1dd6907ac16f9427
odonML <odon_ml@outlook.com>
2021-10-07 19:58:43 -0500

    reverse Kodemia

--- clase3/js/main.js
+++ clase3/js/main.js
@@ -110,3 +110,13 @@ Imprimir en consola el siguiente patrón
 //     console.log(cadenaCamel)
 // }
 // camelCase()
+
+// input: Kodemia
+// output: aimedoK;
+
+function reverse(){
+    let cadena = "Kodemia";
+    let reverse = cadena.split("").reverse().join("");
+    console.log(reverse);
+}
+reverse();
